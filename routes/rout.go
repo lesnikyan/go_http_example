88c4edaa174c609f3e663ff144f9a604e6fc7d73
Sergio Lesnikyan <sergiolesnik@gmail.com>
2020-11-05 11:51:17 +0200

    Upper case name

--- routes/rout.go
+++ routes/rout.go
@@ -15,30 +15,30 @@ type xrout struct {
 	handler http.Handler
 }
 
-type regexpHandler struct {
+type RegexpHandler struct {
 	routes []*xrout
 }
 
 // Handler ...
-func (rh *regexpHandler) Handler(pattern *regexp.Regexp, handler http.Handler) {
+func (rh *RegexpHandler) Handler(pattern *regexp.Regexp, handler http.Handler) {
 	rh.routes = append(rh.routes, &xrout{pattern, handler})
 }
 
 // HandleFunc ...
-func (rh *regexpHandler) HandleFunc(pattern *regexp.Regexp, fhandler func(http.ResponseWriter, *http.Request)) {
+func (rh *RegexpHandler) HandleFunc(pattern *regexp.Regexp, fhandler func(http.ResponseWriter, *http.Request)) {
 	// Handler(pattern, http.HandlerFunc(fhandler))
 	rh.routes = append(rh.routes, &xrout{pattern, http.HandlerFunc(fhandler)})
 }
 
-// HandleFunc ...
-func (rh *regexpHandler) HandleRegexp(pattern string, fhandler func(http.ResponseWriter, *http.Request)) {
+// HandleRegexp ...
+func (rh *RegexpHandler) HandleRegexp(pattern string, fhandler func(http.ResponseWriter, *http.Request)) {
 	// Handler(pattern, http.HandlerFunc(fhandler))
 	rx := regexp.MustCompile(pattern)
 	rh.routes = append(rh.routes, &xrout{rx, http.HandlerFunc(fhandler)})
 }
 
 // ServeHTTP ...
-func (rh *regexpHandler) ServeHTTP(rw http.ResponseWriter, rq *http.Request) {
+func (rh *RegexpHandler) ServeHTTP(rw http.ResponseWriter, rq *http.Request) {
 	for _, rout := range rh.routes {
 		if rout.pattern.MatchString(rq.URL.Path) {
 			rout.handler.ServeHTTP(rw, rq)
